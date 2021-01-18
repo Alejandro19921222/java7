bdcad25bbd5f35c5229de5df9076e98e5c166c83
丹尾州満 <s.nio@tannooshuumannoMacBook-Air.local>
2021-01-18 19:59:41 +0900

    [finish]1,2

---
+++ app.js
@@ -0,0 +1,34 @@
+var hello = "Hello,World";
+
+var str1 = "Hello";
+var str2 = "World!!";
+
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+  ("みかんの値段がりんごより安い");
+}else if(orange == apple){
+  ("みかんとりんごが同じ値段");
+}else{
+  ("みかんの値段がりんごより高い");
+}
+var max = 100;
+var num = 1;
+var count = 0;
+
+while(num < max){
+  num = num + 2;
+  count = count + 1;
+}
+
+
+var x = 1 + 10;
+var y = 5 - 2;
+var z = 7 * 3;
+var a = 5 / 5;
+alert( "1+10=" + x);
+alert( "5-2=" + y);
+alert( "7x3=" + z);
+alert( "5÷5=" + a);
+
